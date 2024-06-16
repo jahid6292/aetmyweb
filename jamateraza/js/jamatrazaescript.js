2a1f3f86cc33ce992728d9e39eefc9ac34537ecf
jahid6292 <95475500+jahid6292@users.noreply.github.com>
2024-06-16 20:47:39 +0530

    Update jamatrazaescript.js

--- jamateraza/js/jamatrazaescript.js
+++ jamateraza/js/jamatrazaescript.js
@@ -409,21 +409,21 @@ function adminfundingmonthwisedetails(useid){
          var stfpanel = document.querySelector('.students-panel');
 
 
-        function myFunction(x) {
-          if (x.matches) { // If media query matches
-            marques.setAttribute("direction", "up");
-            marques.removeAttribute("behavior");
-            stmarques.setAttribute("direction", "up");
-            stfpanel.style.height ="400px";
-            stfpanel.style.overflow = "scroll";
-            stmarques.style.height ="800px";
-          } else {
-           return false;
-          }
-        }
+        // function myFunction(x) {
+        //   if (x.matches) { // If media query matches
+        //     marques.setAttribute("direction", "up");
+        //     marques.removeAttribute("behavior");
+        //     stmarques.setAttribute("direction", "up");
+        //     stfpanel.style.height ="400px";
+        //     stfpanel.style.overflow = "scroll";
+        //     stmarques.style.height ="800px";
+        //   } else {
+        //    return false;
+        //   }
+        // }
 
         var x = window.matchMedia("(max-width: 720px)")
-        myFunction(x) // Call listener function at run time
+        // myFunction(x) // Call listener function at run time
         x.addListener(myFunction) // Attach listener function on state changes
 
         document.getElementById('logout').style.display = 'none';
